bab9924e69d823aac86708e6469e80a37fd73f84
Ameziel <94611725+Ameziel@users.noreply.github.com>
2023-08-23 09:13:48 +0200

    Data table working (#3)
    * Récupération des données "eleves" + premier affichage
    
    * mat table v0.9 works
    
    * up
    
    * Avancement mat table

--- src/app/eleves/eleves/eleves.component.ts
+++ src/app/eleves/eleves/eleves.component.ts
@@ -35,8 +35,8 @@ export class ElevesComponent implements AfterViewInit {
     // const filterValue = (event.target as HTMLInputElement).value;
     // this.dataSource.filter = filterValue.trim().toLowerCase();
 
-  //   // if (this.dataSource.paginator) {
-  //   //   this.dataSource.paginator.firstPage();
+    //   // if (this.dataSource.paginator) {
+    //   //   this.dataSource.paginator.firstPage();
     // }
   }
 }
