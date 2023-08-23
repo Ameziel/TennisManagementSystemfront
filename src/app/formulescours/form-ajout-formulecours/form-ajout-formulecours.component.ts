16b34c069ab82c121a8a829d5d42a4e2085dd0f9
ameziel <samuel.sicard.it@gmail.com>
2023-08-24 00:41:23 +0200

    Ajout des fichiers

---
+++ src/app/formulescours/form-ajout-formulecours/form-ajout-formulecours.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-ajout-formulecours',
+  templateUrl: './form-ajout-formulecours.component.html',
+  styleUrls: ['./form-ajout-formulecours.component.css']
+})
+export class FormAjoutFormulecoursComponent {
+
+}
