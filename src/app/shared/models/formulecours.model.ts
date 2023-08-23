43d2d53829bb54ca730b2764fe66195d6e569b6a
ameziel <samuel.sicard.it@gmail.com>
2023-08-24 00:44:45 +0200

    clean formulecours.model.ts

--- src/app/shared/models/formulecours.model.ts
+++ src/app/shared/models/formulecours.model.ts
@@ -1,15 +1,39 @@
+enum CategorieAge {
+  Enfant = "ENFANT",
+  Jeune = "JEUNE",
+  Adolescent = "ADOLESCENT",
+  Adulte = "ADULTE",
+  Entraineur = "ENTRAINEUR"
+}
+enum Periode{
+  Ete = "ETE",
+    Hiver = "HIVER",
+    Annee = "ANNEE"
+}
+enum TypeBalle {
+  Rouge = "ROUGE",
+  Orange = "ORANGE",
+  Verte = "VERTE",
+  Jaune = "JAUNE"
+}
+enum Niveau {
+  Loisir = "LOISIR",
+  QuatriemeSerie = "QUATRIEME_SERIE",
+  TroisiemeSerie = "TROISIEME_SERIE",
+  DeuxiemeSerie = "DEUXIEME_SERIE"
+}
 export interface Formulecours {
   id: string;
-  libelle : string;
   effectifMaximum : number;
   nombreSeanceTotal : number;
   nombreSeanceHebdomadaire : number;
-  dureeSeanceMinute : number; 
+  dureeSeanceMinuteCours : number;
   tarif: number;
-  annee : number;
   dateDebut : string;
   dateFin : string;
-  periode : string;
-  typeBalle: string;
+  categorieAge : CategorieAge;
+  periode : Periode;
+  typeBalle: TypeBalle;
+  niveau : Niveau;
   actif: boolean;
 }
