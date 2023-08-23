986718264390392d065fbe52ff4ff67b20cb8058
ameziel <samuel.sicard.it@gmail.com>
2023-08-23 12:41:25 +0200

    Affichage du tableau des données des moniteurs

--- src/app/moniteurs/moniteurs/moniteurs.component.ts
+++ src/app/moniteurs/moniteurs/moniteurs.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MoniteursService} from "../../shared/service/moniteurs.service";
 
 @Component({
   selector: 'app-moniteurs',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./moniteurs.component.css']
 })
 export class MoniteursComponent {
+  displayedColumns: string[] = ["nom","prenom","email","telephone", "qualification", "edit"];
+  // dataSource: MatTableDataSource<Eleve>;
+  readonly moniteurs$ = this.moniteurService.getAllMoniteurs();
 
+
+  // @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  // @ViewChild(MatSort) sort: MatSort | undefined;
+  constructor(private moniteurService : MoniteursService) {
+    // Assign the data to the data source for the table to render
+    // this.dataSource = new MatTableDataSource(this.eleves);
+  }
+
+
+  ngAfterViewInit() {
+    // @ts-ignore
+    // this.dataSource.paginator = this.paginator;
+    // @ts-ignore
+    // this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    // const filterValue = (event.target as HTMLInputElement).value;
+    // this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    //   // if (this.dataSource.paginator) {
+    //   //   this.dataSource.paginator.firstPage();
+    // }
+  }
 }
