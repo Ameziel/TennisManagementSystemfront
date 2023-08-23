515d8cbcd0a83d7254b1e08aafd155e118e7bb92
ameziel <samuel.sicard.it@gmail.com>
2023-08-23 12:41:47 +0200

    correction de la variable d'environnement pour les appels du servce moniteur

--- src/app/shared/service/moniteurs.service.ts
+++ src/app/shared/service/moniteurs.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {environment} from "../../environments/environment";
 import {Moniteur} from "../models/moniteur.model";
+import {environment} from "../../../environments/environments";
 
 @Injectable({
   providedIn: 'root'
