c5feb74d45a8915ff21842c61f873ffa22860fd3
ameziel <samuel.sicard.it@gmail.com>
2023-08-24 00:45:41 +0200

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
+import {MAT_DATE_LOCALE} from '@angular/material/core';
 import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
@@ -32,4 +33,5 @@ import {HttpClientModule} from "@angular/common/http";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
