4078fea6555179c661a3d6d6c7b484b7e47906d6
ameziel <samuel.sicard.it@gmail.com>
2023-08-25 15:05:59 +0200

    Ajout du service prestationcours

---
+++ src/app/shared/service/prestationscours.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environments";
+import {PrestationCours} from "../models/prestationCours";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrestationscoursService {
+  constructor(private http: HttpClient) {
+  }
+
+  public getAllPrestationsCours(): Observable<Array<PrestationCours>> {
+    return this.http.get<Array<PrestationCours>>(environment.backendHost + "/prestationscours")
+  }
+
+  public getPrestationCoursById(id: string): Observable<PrestationCours> {
+    return this.http.get<PrestationCours>(environment.backendHost + "/prestationscours/" + id);
+  }
+
+  public savePrestationCours(groupeCours: PrestationCours): Observable<PrestationCours> {
+    return this.http.post<PrestationCours>(environment.backendHost + "/prestationscours/" + groupeCours.id, groupeCours);
+  }
+
+  public updatePrestationCours(prestationCours: PrestationCours): Observable<PrestationCours> {
+    return this.http.put<PrestationCours>(environment.backendHost + "/prestationscours", prestationCours);
+  }
+
+  public deletePrestationCours(id: string) {
+    return this.http.delete(environment.backendHost + "/groupescours/" + id);
+  }
+}
