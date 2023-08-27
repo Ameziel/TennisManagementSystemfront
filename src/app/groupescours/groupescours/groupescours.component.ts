393ce3c82d04989412a8bef788b464b556b3f1a6
ameziel <samuel.sicard.it@gmail.com>
2023-08-28 00:48:34 +0200

    Ajout des fonctionalité d'ajout et d'édition des groupes

--- src/app/groupescours/groupescours/groupescours.component.ts
+++ src/app/groupescours/groupescours/groupescours.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Observable} from "rxjs";
 import {GroupeCours} from "../../shared/models/groupeCours";
 import {GroupescoursService} from "../../shared/service/groupescours.service";
+import {FormEditGroupecoursComponent} from "../form-edit-groupecours/form-edit-groupecours.component";
+import {MatDialog} from "@angular/material/dialog";
+import {FormAjoutGroupecoursComponent} from "../form-ajout-groupecours/form-ajout-groupecours.component";
 
 @Component({
   selector: 'app-groupescours',
@@ -12,10 +15,25 @@ export class GroupescoursComponent {
 
   groupesCours!: Observable<Array<GroupeCours>>;
 
-  constructor(private groupesCoursService: GroupescoursService) {
+  constructor(private groupesCoursService: GroupescoursService,
+              private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
     this.groupesCours = this.groupesCoursService.getAllGroupesCours()
+  }
+
+  openAjoutGroupeCoursFormDialog() {
+    const dialogRef = this.dialog.open(FormAjoutGroupecoursComponent, {width: "1100px", height: "80vh"});
+    dialogRef.afterClosed();
+  }
+
+  openEditGroupeCoursFormDialog(groupeCours: GroupeCours) {
+    const dialogRef = this.dialog.open(FormEditGroupecoursComponent, {
+      width: "1100px",
+      height: "80vh",
+      data: groupeCours
+    });
+    dialogRef.afterClosed();
   }
 }
