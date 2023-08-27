25182c66b0dba0218b0f44800abf378305e8cf1f
ameziel <samuel.sicard.it@gmail.com>
2023-08-28 01:18:20 +0200

    v2 pour la maj dynamique du tableau

--- src/app/prestationscours/prestationscours/prestationscours.component.ts
+++ src/app/prestationscours/prestationscours/prestationscours.component.ts
@@ -2,27 +2,36 @@ import {Component, OnInit} from '@angular/core';
 import {PrestationscoursService} from "../../shared/service/prestationscours.service";
 import {MoniteursService} from "../../shared/service/moniteurs.service";
 import {Moniteur} from "../../shared/models/moniteur.model";
+import {MatDialog} from "@angular/material/dialog";
+import {PrestationCours} from "../../shared/models/prestationCours";
+import {FormEditPrestationcoursComponent} from "../form-edit-prestationcours/form-edit-prestationcours.component";
+import {FormAjoutPrestationscoursComponent} from "../form-ajout-prestationcours/form-ajout-prestationscours.component";
 
 @Component({
   selector: 'app-prestationscours',
   templateUrl: './prestationscours.component.html',
   styleUrls: ['./prestationscours.component.css']
 })
-export class PrestationscoursComponent implements OnInit{
+export class PrestationscoursComponent implements OnInit {
 
   displayedColumns: string[] = ["datePrestation", "dureeSeanceMinutePrestation", "moniteur", "commentaire", "edit"];
-
-  readonly prestationsCours$ = this.prestationsCoursService.getAllPrestationsCours();
   listMoniteurs: Moniteur[] = [];
+  listPrestations: PrestationCours[] = [];
 
-  constructor(private prestationsCoursService: PrestationscoursService, private moniteurService: MoniteursService) {
+  constructor(private prestationsCoursService: PrestationscoursService,
+              private moniteurService: MoniteursService,
+              private dialog: MatDialog) {
   }
 
   ngOnInit() {
-    this.moniteurService.getAllMoniteurs().subscribe(moniteur => {
-      this.listMoniteurs = moniteur;
+    this.moniteurService.getAllMoniteurs().subscribe(moniteurs => {
+      this.listMoniteurs = moniteurs;
     });
+    this.prestationsCoursService.getAllPrestationsCours().subscribe(prestations => {
+      this.listPrestations = prestations
+    })
   }
+
   ngAfterViewInit() {
     // @ts-ignore
     // this.dataSource.paginator = this.paginator;
@@ -39,4 +48,25 @@ export class PrestationscoursComponent implements OnInit{
     // }
   }
 
+  openEditPrestationCoursFormDialog(prestationCours: PrestationCours) {
+    const dialogRef = this.dialog.open(FormEditPrestationcoursComponent, {
+      width: "1100px",
+      height: "80vh",
+      data: prestationCours
+    });
+    dialogRef.afterClosed().subscribe(() => {  //avoid
+      this.prestationsCoursService.getAllPrestationsCours().subscribe(
+        prestations => this.listPrestations = prestations
+      )
+    });
+  }
+
+  openAjoutPrestationCoursFormDialog() {
+    const dialogRef = this.dialog.open(FormAjoutPrestationscoursComponent, {width: "1100px", height: "80vh"});
+    dialogRef.afterClosed().subscribe(() => {  //avoid
+      this.prestationsCoursService.getAllPrestationsCours().subscribe(
+        prestations => this.listPrestations = prestations
+      )
+    });
+  }
 }
