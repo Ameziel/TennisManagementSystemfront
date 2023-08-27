ae3bd2c9f148d5e4cc5c0575b4b1875491cb2540
ameziel <samuel.sicard.it@gmail.com>
2023-08-28 00:20:38 +0200

    up

--- src/app/inscriptionsformule/inscriptionsformule/inscriptionsformule.component.ts
+++ src/app/inscriptionsformule/inscriptionsformule/inscriptionsformule.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import {InscriptionsformuleService} from "../../shared/service/inscriptionsformule.service";
+import {FormAjoutComponent} from "../../eleves/form-ajout/form-ajout.component";
+import {
+  FormAjoutInscriptionformuleComponent
+} from "../form-ajout-inscriptionformule/form-ajout-inscriptionformule.component";
+import {
+  FormEditInscriptionsformuleComponent
+} from "../form-edit-inscriptionsformule/form-edit-inscriptionsformule.component";
+import {MatDialog} from "@angular/material/dialog";
+import {InscriptionFormule} from "../../shared/models/inscriptionFormule";
 
 @Component({
   selector: 'app-inscriptionsformule',
@@ -10,7 +19,17 @@ export class InscriptionsformuleComponent {
 
   readonly inscriptionsFormule$ = this.inscriptionsFormuleService.getAllInscriptionsFormule();
 
-  constructor(private inscriptionsFormuleService : InscriptionsformuleService) {
+  constructor(private inscriptionsFormuleService : InscriptionsformuleService,
+              private dialog: MatDialog) {
   }
 
+  openAjoutFormuleFormDialog() {
+    const dialogRef = this.dialog.open(FormAjoutInscriptionformuleComponent, {width: "1100px", height: "80vh"});
+    dialogRef.afterClosed();
+  }
+
+  openEditInscriptionFormuleFormDialog(inscriptionFormule: InscriptionFormule) {
+    const dialogRef = this.dialog.open(FormEditInscriptionsformuleComponent, {width: "1100px", height: "80vh", data: inscriptionFormule});
+    dialogRef.afterClosed();
+  }
 }
