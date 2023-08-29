336ceb781ab9d27e20e640fbaff301bc17923628
ameziel <samuel.sicard.it@gmail.com>
2023-08-29 21:10:40 +0200

    Fix import and using of echarts

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MainPageComponent} from './main-page/main-page.component';
 import {RouterModule, Routes} from "@angular/router";
+import {NgxEchartsModule} from "ngx-echarts";
 
 
 const routes: Routes = [
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    NgxEchartsModule,
   ]
 })
 export class DashboardModule {
