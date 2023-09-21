48e2186ddcaf1926d42edf7b83b12157cb9871a0
ameziel <samuel.sicard.it@gmail.com>
2023-09-21 21:55:09 +0200

    Création du fichier "forms-message"

---
+++ src/app/shared/constants/forms-messages.ts
@@ -0,0 +1,4 @@
+export const REQUIRED_FIELD_MESSAGE = "Le champ est requis";
+export const INVALID_NAME_PATTERN = "Format nom/prénom invalide";
+export const INVALID_EMAIL_PATTERN = "Format email invalide";
+export const INVALID_PHONENUMBER_PATTERN = "Format telephone invalide";
