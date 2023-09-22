79c9f7446d57b82eedda492a682791d4950fdbf9
ameziel <samuel.sicard.it@gmail.com>
2023-09-22 19:56:49 +0200

    fix graphique

--- src/app/dashboard/main-page/main-page.component.ts
+++ src/app/dashboard/main-page/main-page.component.ts
@@ -13,7 +13,7 @@ export class MainPageComponent implements OnInit{
   ngOnInit(): void {
     this.options = {
       title : {
-        text: "Évolutions et répartitions des élèves par âge",
+        text: "Répartition des élèves par âge",
         show: true,
       },
       legend: {},
@@ -23,7 +23,7 @@ export class MainPageComponent implements OnInit{
       },
       dataset: {
         source: [
-          ['Eleves actuellement inscritse', '2012', '2013', '2014', '2015', '2016', '2017'],
+          ['Eleves actuellement inscritse', '2016', '2017', '2018', '2019', '2020', '2021'],
           ['30ans et +', 56.5, 82.1, 88.7, 70.1, 53.4, 90.1],
           ['50ans et +', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
           ['[15;20]ans', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
@@ -60,12 +60,12 @@ export class MainPageComponent implements OnInit{
             focus: 'self'
           },
           label: {
-            formatter: '{b}: {@2012} ({d}%)'
+            formatter: '{b}: {@2016} ({d}%)'
           },
           encode: {
             itemName: 'product',
-            value: '2012',
-            tooltip: '2012'
+            value: '2016',
+            tooltip: '2016'
           }
         }
       ]
