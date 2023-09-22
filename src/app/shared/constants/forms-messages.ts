fb001ec068a63b36d539c4e8fe2c364dbbb02129
ameziel <samuel.sicard.it@gmail.com>
2023-09-22 19:56:56 +0200

    up

--- src/app/shared/constants/forms-messages.ts
+++ src/app/shared/constants/forms-messages.ts
@@ -2,3 +2,8 @@ export const REQUIRED_FIELD_MESSAGE = "Le champ est requis";
 export const INVALID_NAME_PATTERN = "Format nom/prénom invalide";
 export const INVALID_EMAIL_PATTERN = "Format email invalide";
 export const INVALID_PHONENUMBER_PATTERN = "Format telephone invalide";
+export const INVALID_MONTANT_VALUE = "Le montant doit être positif";
+export const ERR_CHOIX_ELEVE_INEXISTANT = "Vous devez choisir un élève";
+export const ERR_CHOIX_GROUPE_INEXISTANT = "Vous devez choisir un groupe";
+export const ERR_CHOIX_FORMULE_INEXISTANT = "Vous devez choisir une formule";
+
