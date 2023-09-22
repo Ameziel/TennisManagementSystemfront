951ba84e66c502d07d23b0a0a767b6dcf1b1081b
ameziel <samuel.sicard.it@gmail.com>
2023-09-22 19:19:43 +0200

    Ajout des erreurs personnalisées dans le formulaire

--- src/app/eleves/form-edit/form-edit.component.ts
+++ src/app/eleves/form-edit/form-edit.component.ts
@@ -3,6 +3,12 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Eleve} from 'src/app/shared/models/eleve.model';
 import {ElevesService} from "../../shared/service/eleves.service";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {
+  INVALID_EMAIL_PATTERN,
+  INVALID_NAME_PATTERN, INVALID_PHONENUMBER_PATTERN,
+  REQUIRED_FIELD_MESSAGE
+} from "../../shared/constants/forms-messages";
+import {NAME_REGEX} from "../../shared/constants/regexs";
 
 @Component({
   selector: 'app-form-ajout',
@@ -13,6 +19,10 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 export class FormEditComponent implements OnInit {
   maxDate: Date = new Date();
   editEleveFormGroup!: FormGroup;
+  err_message_required = REQUIRED_FIELD_MESSAGE;
+  err_message_name_pattern= INVALID_NAME_PATTERN;
+  err_message_email_pattern = INVALID_EMAIL_PATTERN;
+  err_message_phoneNumber_pattern = INVALID_PHONENUMBER_PATTERN;
 
   constructor(private eleveService: ElevesService,
               private formBuilder: FormBuilder,
@@ -24,12 +34,12 @@ export class FormEditComponent implements OnInit {
     this.editEleveFormGroup = this.formBuilder.group(
       {
         id: this.formBuilder.control(null, [Validators.required]),
-        prenom: this.formBuilder.control(null, [Validators.required]),
-        nom: this.formBuilder.control(null, [Validators.required]),
+        prenom: this.formBuilder.control(null, [Validators.required, Validators.pattern(NAME_REGEX)]),
+        nom: this.formBuilder.control(null, [Validators.required, Validators.pattern(NAME_REGEX)]),
         genre: this.formBuilder.control(null),
-        email: this.formBuilder.control(null, [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+        email: this.formBuilder.control(null, [Validators.required,Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
         telephone: this.formBuilder.control(null, [Validators.required]),
-        dateDeNaissance: this.formBuilder.control([Validators.required, Validators.pattern("^[0-9]{10}$"), Validators.minLength(10), Validators.maxLength(10)]),
+        dateDeNaissance: this.formBuilder.control([Validators.required]),
         details: this.formBuilder.control(null),
         actif: this.formBuilder.control(false),
       })
